413bd590b53827720d451eb3d61fb95b26972719
Chris Saden <chris.saden@udacity.com>
2017-10-27 01:37:34 -0700

    feature(colors): add new colors

--- utils/colors.js
+++ utils/colors.js
@@ -3,4 +3,7 @@ export const teal = '#1C5C5F';
 export const green = '#2B8470';
 export const limeGreen = '#9CCF6D';
 export const yellow = '#EFEE69';
+export const grey = '#7E97AF';
+export const lightGrey = '#DBE2E8';
 export const white = '#FFF';
+export const macBlue = '#0E7AFE';
